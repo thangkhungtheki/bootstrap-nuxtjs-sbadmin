9d8de01ebd380da9f136c73aae68b41a32c2a7ee
tktk <thangkhungtheki@gmail.com>
2024-05-14 16:31:49 +0700

    action ,theme, vuex

--- pages/user/index.vue
+++ pages/user/index.vue
@@ -1,73 +1,95 @@
 '
 <template>
   <!-- <b-table :items="items" :fields="fields"></b-table> -->
-  <div>
-    <b-container class='containerTable'>
-      
-        <b-table-lite hover small caption-top responsive :items="fataa" :fields="fields" />
+ 
 
-    
-      
-    
-    </b-container>
+  <div>
+    <h2>Counter: {{ counter }}</h2>
+    <hr/>
+    <b-button variant="success" @click="handletang">TĂNG</b-button>
+    <b-button variant="danger" @click="handlegiam">GIẢM</b-button>
+    <hr/>
+    <b-card header-tag="header" footer-tag="footer" header="Table" body-class="p-0 card-fixed">
+      <b-table hover :fields="fields"></b-table>
+    </b-card>
   </div>
+
+
+
+
 </template>
 
 <script>
-  export default {
-    async asyncData() {
-      try {
-        const response = await fetch('http://127.0.0.1:8000/users')
-        const fata = await response.json()
-        return {
-          fataa: fata ? fata : []
-        }
-      } catch (error) {
-        return {
-          fataa: []
-        }
-      }
+import {mapState, mapActions} from 'vuex'
+export default {
 
-    },
-    layout: "areaAdmin",
-    data() {
+  async asyncData() {
+    try {
+      const response = await fetch('http://127.0.0.1:8000/users')
+      const fata = await response.json()
       return {
-
-
-        fields: [{
-            key: "id",
-            sortable: true,
-          },
-          {
-            key: "name",
-            // label: "Last name",
-            sortable: true,
-          },
-          {
-            key: "email",
-            // label: "Age",
-            sortable: true,
-          },
-          {
-            key: "phan4",
-            sortable: true,
-          },
-          {
-            key: "phan5",
-            sortable: true,
-          },
-          {
-            key: "phan6",
-            sortable: true,
-          },
-        ],
+        fataa: fata ? fata : []
       }
-    },
-  }
-</script>
+    } catch (error) {
+      return {
+        fataa: []
+      }
+    }
 
-<style>
-.containerTable {
-  margin: 0%;
+  },
+  computed: {
+    ...mapState({
+      counter: state => state.counter,
+    })
+  },
+  methods: {
+    ...mapActions({
+      add: 'increment',
+      except: 'decrement'
+    }),
+    handletang(){
+      this.add()
+    },
+    handlegiam(){
+      this.except()
+    }
+  },
+
+  layout: "areaAdmin",
+  data() {
+    return {
+      items: [
+
+      ],
+      fields: [
+        {
+          key: "first_name",
+          label: "Person Name",
+          sortable: true,
+        },
+        {
+          key: "last_name",
+          label: "last_name",
+          sortable: true,
+        },
+        {
+          key: "age",
+          label: "age",
+          sortable: true,
+        },
+        {
+          key: "BON",
+          label: "bon",
+          sortable: true,
+        },
+        {
+          key: "NAM",
+          label: "nam",
+          sortable: true,
+        },
+
+      ],
+    }
+  },
 }
-</style>
+</script>
--- store/index.js
+++ store/index.js
@@ -1,18 +1,37 @@
 export const state = () => ({
     activeclass: "vertical-collpsed",
-    menuclosed:true
-  })
-  
-  export const mutations = {
+    menuclosed: true,
+    counter: 0
+})
+
+export const mutations = {
     toggle(state) {
-        if(state.activeclass=="vertical-collpsed"){
-            state.activeclass="";
-        }else{
-            state.activeclass="vertical-collpsed";
+        if (state.activeclass == "vertical-collpsed") {
+            state.activeclass = "";
+        } else {
+            state.activeclass = "vertical-collpsed";
         }
     },
-    menuToggle(state){
+    menuToggle(state) {
         state.menuclosed = state.menuclosed ? false : true;
-    }
-  }
+    },
+
+    increment(state) {
+        state.counter++
+    },
+    
+    decrement(state) {
+        state.counter--
+    },
+
+}
+
+export const actions = {
+    increment(context) {
+        context.commit('increment')
+    },
+    decrement(context) {
+        context.commit('decrement')
+    },
+}
 
