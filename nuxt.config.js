8f337df7ea98773192e8e22e18b9f0c03203939f
thangkhungtheki <thangkhungtheki@gmail.com>
2024-06-03 16:45:51 +0700

    update socket-io

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+   '~/plugins/socket.js'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
---
+++ plugins/socket.js
@@ -0,0 +1,16 @@
+// plugins/socket.js
+import { io } from 'socket.io-client';
+
+export default ({ store }, inject) => {
+//   const socket = io('http://127.0.0.1:4000'); // Địa chỉ server của bạn
+
+//   socket.on('connect', () => {
+//     console.log('Connected to server');
+//   });
+
+//   socket.on('update', (data) => {
+//     store.commit('updateData', data);
+//   });
+
+//   inject('socket', socket);
+}
