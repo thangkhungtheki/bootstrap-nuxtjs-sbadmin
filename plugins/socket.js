40fc533a0efc0ed16525a26e53772633cdb523e2
tktk <thangkhungtheki@gmail.com>
2024-07-04 17:00:27 +0700

    socket

--- plugins/socket.js
+++ plugins/socket.js
@@ -2,7 +2,7 @@ import io from 'socket.io-client'
 
 const socket = io(process.env.SOCKET_URL,{
     withCredentials: true,
-    Headers: {
+    extraHeaders: {
           "my-custom-header": "abcd"
     }
 })
